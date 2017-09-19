6e44aef0de51b5bb8946618aa5e3695dbed3e45b
Ninad Kamat <nkamat@andrew.cmu.edu>
2017-09-18 22:58:20 -0400

    Port project to CMake

---
+++ include/ysgl/glsl_src/plain2DDrawingVertexShaderSrc.glsl
@@ -0,0 +1,17 @@
+#include "YS_GLSL_HEADER.glsl"
+
+uniform  HIGHP  mat4 transform;
+
+attribute LOWP  vec2 vertex;
+attribute LOWP  vec4 colorIn;
+attribute MIDP  float pointSizeIn;
+
+varying LOWP  vec4 colorOut;
+
+void main()
+{
+	vec4 vertex4d=vec4(vertex,0,1);
+	gl_Position=transform*vertex4d;
+	colorOut=colorIn;
+	gl_PointSize=pointSizeIn;
+}
